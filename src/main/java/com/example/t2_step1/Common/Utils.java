a1bcea831a8d6a8f25912e2bb45d30e7627ff918
Pay-Pulse Company <31178314+paypulse@users.noreply.github.com>
2023-08-22 09:03:20 +0900

    api url 호출 로직

--- src/main/java/com/example/t2_step1/Common/Utils.java
+++ src/main/java/com/example/t2_step1/Common/Utils.java
@@ -1,9 +1,80 @@
 package com.example.t2_step1.Common;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Utils {
 
-    public static
+    public static void InitData(String realUrl, String serviceKey, String mobileOs, String mobileApp ){
 
+        URL url = null;
+        HttpURLConnection urlConnection= null;
+
+        int connTimeout = 5000;
+        int readTimeout = 9000;
+        int responseCode = 0;
+
+        String urlT = realUrl +"?MobileOS="+mobileOs+"&MobileApp="+mobileApp+"&serviceKey="+serviceKey;
+
+
+        try {
+
+
+            url = new URL(urlT );
+
+
+            System.out.println(url);
+
+            urlConnection = (HttpURLConnection)url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setConnectTimeout(connTimeout);
+            urlConnection.setReadTimeout(readTimeout);
+
+            urlConnection.setRequestProperty("Accept", "application/json;");
+
+            responseCode = urlConnection.getResponseCode();
+
+            if(responseCode == 200){
+                StringBuilder sb = new StringBuilder();
+                String line = "";
+                InputStreamReader ir = new InputStreamReader(urlConnection.getInputStream());
+                BufferedReader br = new BufferedReader(ir);
+                while( (line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+
+    public StringBuilder readResopnseData(InputStream in) {
+        if(in == null ) return null;
+
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+
+        try (InputStreamReader ir = new InputStreamReader(in);
+             BufferedReader br = new BufferedReader(ir)){
+            while( (line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return sb;
+    }
 
 
 }
--- src/main/java/com/example/t2_step1/Gallery/GalleryApi.java
+++ src/main/java/com/example/t2_step1/Gallery/GalleryApi.java
@@ -3,19 +3,40 @@ package com.example.t2_step1.Gallery;
 
 import com.example.t2_step1.Common.CommonRes;
 import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import static com.example.t2_step1.Common.Utils.InitData;
+
+
 @Controller
 public class GalleryApi {
+
+
+    @Value("${openApi.sample.url}")
+    private  String realUrl;
+
+    @Value("${openApi.sample.serviceKey}")
+    private String serviceKey;
+
+    @Value("${openApi.sample.mobileOs}")
+    private String mobileOs;
+
+    @Value("${openApi.sample.mobileApp}")
+    private String mobileApp;
+
+
 
     @Operation(summary = "Api조회 후 insert")
     @PostMapping("/initData")
     public ResponseEntity<CommonRes> initData(){
 
 
+        //data api call
+        InitData(realUrl, serviceKey, mobileOs, mobileApp);
 
 
 
